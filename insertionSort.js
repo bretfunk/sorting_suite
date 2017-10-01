98abcf5bba721d6bcdbcb7d438e226454d8bbc0f
BFunk <bret.e.funk@gmail.com>
2017-10-01 17:31:20 -0600

    stuck but playing with it

--- insertionSort.js
+++ insertionSort.js
@@ -1,19 +1,21 @@
 function insertionSort(array) {
-  //return array.sort(function(a,b){
-  //return a - b;
+  let arrayLength = array.length
   let sortedArray = [];
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i <= arrayLength; i++) {
     console.log("i" + i);
     let removed = array.shift();
       if (sortedArray.length === 0) {
         sortedArray.unshift(removed);
       } else {
-        for (let j = 0; j < sortedArray.length; j++) {
+        for (let j = 0; j <= sortedArray.length; j++) {
           console.log("j" + j);
           if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null)) {
             sortedArray.splice((j + 1), 0, removed);
             console.log(sortedArray);
-            break;
+            j = 0;
+          } else {
+            console.log("else");
+            j = 0;
           };
         };
       };
