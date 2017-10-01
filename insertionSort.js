925c75cfd91fe59593f1d7cdf068788ac361c1ff
BFunk <bret.e.funk@gmail.com>
2017-10-01 17:09:30 -0600

    fixing bugs

--- insertionSort.js
+++ insertionSort.js
@@ -3,17 +3,25 @@ function insertionSort(array) {
   //return a - b;
   let sortedArray = [];
   for (let i = 0; i < array.length; i++) {
-    let removed = array.shift
+    console.log("i" + i);
+    let removed = array.shift();
       if (sortedArray.length === 0) {
         sortedArray.unshift(removed);
       } else {
         for (let j = 0; j < sortedArray.length; j++) {
-          if (sortedArray[j] >= removed && ((sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null))
-              sortedArray.splice((j + 1), 1, removed);
-        }
-      }
-  });
+          console.log("j" + j);
+          if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null)) {
+            sortedArray.splice((j + 1), 0, removed);
+            console.log(sortedArray);
+            break;
+          };
+        };
+      };
+  };
   return sortedArray;
 };
+
+let testArray = [7, 2, 4, 10];
+console.log(insertionSort(testArray));
 
 module.exports = insertionSort
