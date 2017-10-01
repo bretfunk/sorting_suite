e3dfe3bc44529c9afcd6d117576fbdb54bb2bd88
BFunk <bret.e.funk@gmail.com>
2017-10-01 17:50:33 -0600

    started looking at solutions online and it looks like my second for method is the issue, still playing with it

--- insertionSort.js
+++ insertionSort.js
@@ -7,18 +7,20 @@ function insertionSort(array) {
       if (sortedArray.length === 0) {
         sortedArray.unshift(removed);
       } else {
-        for (let j = 0; j <= sortedArray.length; j++) {
+        for (let j = i - 1; j >= 0 && (sortedArray[j] > removed); j--) {
           console.log("j" + j);
-          if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null)) {
-            sortedArray.splice((j + 1), 0, removed);
-            console.log(sortedArray);
-            j = 0;
-          } else {
-            console.log("else");
-            j = 0;
-          };
+          //if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null)) {
+            //sortedArray.splice((j + 1), 0, removed);
+            //console.log(sortedArray);
+            //if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed)) {
+              sortedArray[j + 1] = sortedArray[j];
+              sortedArray[j] = removed;
+          //} else {
+            //console.log("else");
+          //};
         };
       };
+
   };
   return sortedArray;
 };
