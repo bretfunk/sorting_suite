9fed06bc7051ebe0ebc049af6c65ee64aa540e15
BFunk <bret.e.funk@gmail.com>
2017-10-01 16:54:57 -0600

    testing out insertion sort ideas

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,19 @@
 function insertionSort(array) {
-  return array.sort(function(a,b){
-  return a - b;
-});
+  //return array.sort(function(a,b){
+  //return a - b;
+  let sortedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let removed = array.shift
+      if (sortedArray.length === 0) {
+        sortedArray.unshift(removed);
+      } else {
+        for (let j = 0; j < sortedArray.length; j++) {
+          if (sortedArray[j] >= removed && ((sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null))
+              sortedArray.splice((j + 1), 1, removed);
+        }
+      }
+  });
+  return sortedArray;
 };
 
 module.exports = insertionSort
