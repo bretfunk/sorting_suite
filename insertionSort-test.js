9f8de098cfc54814b6352225e174ab796b8b3d54
BFunk <bret.e.funk@gmail.com>
2017-10-01 16:30:11 -0600

    adding insertion sort test

---
+++ insertionSort-test.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert
+var insertionSort = require('./insertionSort')
+
+describe("sorting via insertion", function() {
+  it ("sorts via insertion", function() {
+  });
+  });
