5f97761894a605979b3f5027108e38b4ceb78a43
BFunk <bret.e.funk@gmail.com>
2017-10-01 16:35:59 -0600

    addeded test functionality

--- insertionSort-test.js
+++ insertionSort-test.js
@@ -3,6 +3,15 @@ var insertionSort = require('./insertionSort')
 
 describe("sorting via insertion", function() {
   it ("sorts via insertion", function() {
-    assert.equal(insertionSort(), "it works")
+    assert.deepEqual(insertionSort([5, 1, 10, 4]), [1, 4, 5, 10])
   });
+  it ("sorts via insertion", function() {
+    assert.deepEqual(insertionSort([1, 1, 1, 1]), [1, 1, 1, 1])
   });
+  it ("sorts via insertion", function() {
+    assert.deepEqual(insertionSort([5, 4, 3, 2]), [2, 3, 4, 5])
+  });
+  it ("sorts via insertion", function() {
+    assert.deepEqual(insertionSort([100000, 1, 10, 100]), [1, 10, 100, 100000])
+  });
+});
