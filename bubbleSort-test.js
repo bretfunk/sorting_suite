b12a4c8b8fa1221529c0c44c84a61dd5d0fbf08b
BFunk <bret.e.funk@gmail.com>
2017-10-01 13:28:42 -0600

    working basic test

---
+++ bubbleSort-test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert
+const bubbleSort = require('./bubbleSort')
+
+describe("bubble sort sorts", function() {
+  it("sorts ascending order", function() {
+
+  })
+})
