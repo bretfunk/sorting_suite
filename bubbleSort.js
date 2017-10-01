d3c9e99a2b782991fd1f843dfa4b0646f4adbe6f
BFunk <bret.e.funk@gmail.com>
2017-10-01 15:56:56 -0600

    trying to use splice

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,14 +7,18 @@ function bubbleSort(array) {
     for (var j = 0; j < array.length - i; j++) {
       if (array[j] > array[j + 1]) {
         //array[j] = array[j + 1], array[j + 1] = array[j];
-        let removed = array.slice(j, 2)
-        array.splice(j, 2, removed)
-        eval(pry.it);
+        let removed = array.slice(j, j + 2)
+        array.splice(j, 2, removed.reverse())
+        //eval(pry.it);
 
       };
     };
   };
+  return array;
 };
+
+let testArray = [7, 3, 2, 5];
+console.log(bubbleSort(testArray));
 
 
 module.exports = bubbleSort
