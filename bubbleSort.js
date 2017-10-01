e2c61e0a469982a25f5f15edbc26a07c89ad129b
BFunk <bret.e.funk@gmail.com>
2017-10-01 15:40:25 -0600

    working on bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,19 @@
+var pry = require('pryjs');
 function bubbleSort(array) {
-  return array.sort(function(a,b){
-    return a-b;
-  });
+  //return array.sort(function(a,b){
+    //return a-b;
+  //});
+  for (var i = 0; i < array.lenth; i++) {
+    for (var j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        //array[j] = array[j + 1], array[j + 1] = array[j];
+        let removed = array.slice(j, 2)
+        array.splice(j, 2, removed)
+        eval(pry.it);
+
+      };
+    };
+  };
 };
 
 
