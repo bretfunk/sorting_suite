11451c1526cd70cf7aec96b180dcb76fef739772
BFunk <bret.e.funk@gmail.com>
2017-10-01 18:21:59 -0600

    works 80 percent of the time.  Trying to fix bugs

--- insertionSort.js
+++ insertionSort.js
@@ -2,13 +2,11 @@ function insertionSort(array) {
   let arrayLength = array.length
   let sortedArray = [];
   for (let i = 0; i <= arrayLength; i++) {
-    console.log("i" + i);
     let removed = array.shift();
       if (sortedArray.length === 0) {
         sortedArray.unshift(removed);
       } else {
         for (let j = i - 1; j >= 0 && (sortedArray[j] > removed); j--) {
-          console.log("j" + j);
           //if ((sortedArray[j] >= removed) && (sortedArray[j + 1] <= removed) || (sortedArray[j + 1] === null)) {
             //sortedArray.splice((j + 1), 0, removed);
             //console.log(sortedArray);
